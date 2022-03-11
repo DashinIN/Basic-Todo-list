e60f112b0a368971143f9dfc059f3cafb5d8ac19
DashinIN <green19grant@yandex.ru>
2022-03-11 17:40:31 +0300

    начал делать времена года

--- scripts/script.js
+++ scripts/script.js
@@ -134,3 +134,44 @@ if(localStorage.getItem('todosLocal')) {
         let result = timePlus.toUTCString();  // Переводим в строку UTC;
         console.log(result); // Выводим дату. 
     }
+
+
+ 
+  const buttons =  document.querySelectorAll('.header__menu-item');
+  const buttonsArea = document.querySelector('.header__menu');
+  const bodyBlock = document.querySelector('body');
+  console.log(bodyBlock.style.backgroundImage);
+
+  buttonsArea.addEventListener('click', itemToggle);
+   
+
+    function itemToggle(e) {
+        const item = e.target;
+        for (let button of buttons) {
+            if (button != item) button.classList.remove("active");
+        }
+        item.classList.toggle("active");
+        
+        switch (item.classList[1]) {
+            case 'item-winter':
+                bodyBlock.className = 'winter';
+                break;
+            case 'item-spring':
+                bodyBlock.className = 'spring';
+                break;
+            case 'item-summer':
+                bodyBlock.className = 'summer';
+                break;
+
+            case 'item-autumn':
+                bodyBlock.className = 'autumn';
+                break;
+            default:
+               
+          }
+       
+        
+        
+       
+        console.log(item.classList[2]);
+    }
